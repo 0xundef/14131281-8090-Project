cc5ccb9679e3b5fb86254de8371c26f00c0b895e
cobrazc <zhanchihu@gmail.com>
2026-03-07 11:32:02 +0800

    feat: center main application window on screen

--- src/application.py
+++ src/application.py
@@ -10,7 +10,15 @@ class Application:
             
         self.root = tk.Tk()
         self.root.title("Relax Your Eyes")
-        self.root.geometry("400x300")
+        
+        # Center the window
+        window_width = 400
+        window_height = 300
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        x_cordinate = int((screen_width/2) - (window_width/2))
+        y_cordinate = int((screen_height/2) - (window_height/2))
+        self.root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
         
         # Basic label
         label = tk.Label(self.root, text="Relax Your Eyes Application", font=("Arial", 16))
